a8a4419374c2e4296b765befe52f0aa3e9338f2e
Miguel Tress <u25103001@alumnos.univo.edu.mx>
2025-04-12 12:47:44 -0600

    Se creo el cuesionario inscripcion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from "./formulario/formulario.component";
+import { CuerpoComponent } from "./cuerpo/cuerpo.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormularioComponent, CuerpoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
